d8843f97ba8ffa55c160b1fe9eb07997dbe39f3c
Miguel <114703609+Alendaa@users.noreply.github.com>
2024-03-14 23:31:12 -0300

    Update Parts.lua

--- src/Modules/Parts.lua
+++ src/Modules/Parts.lua
@@ -2,24 +2,16 @@ local module = {}
 module.__index = module
 
 ---@class Part
----@field Type string Type of the part.
 ---@field Position Vector2 Part position.
 
 ---@class Damage: Part
 ---@field Damage number Damage of the part.
 
 ---@return Part | Damage
-function module.new(type, damage)
+function module.new(damage)
     ---@type Part | Damage
-    local part = {
-        Type = type,
-        Position = Vector2.new()
-    }
-
-    if type and type == "Damage" then
-        part.Damage = damage or 25
-    end
-
+    local part = {}
+    part.Damage damage or 25
     part.Position = Vector2.new()
 
     return part
