0bf316cc50cc925ceb0a319f103835ce6d7fd6a6
Miguel <114703609+Alendaa@users.noreply.github.com>
2024-03-14 23:27:47 -0300

    Add files via upload

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+    t.window.title = "Diversão"
+
+    t.window.resizable = true
+    t.console = true
+end
---
+++ src/Game.lua
@@ -0,0 +1,3 @@
+_G.Parts = require("src.Modules.Parts")
+_G.Players = require("src.Modules.Players")
+_G.Vector2 = require("src.Modules.Vector2")
